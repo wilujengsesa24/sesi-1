cf5f14ad120b5d4ea7c63f74fb30f1dc070b9f91
wilujengsesa24 <150676533+wilujengsesa24@users.noreply.github.com>
2023-11-14 14:28:43 +0700

    Add files via upload

---
+++ latijan sesi 1.py
@@ -0,0 +1,15 @@
+a = 4.2
+b = 3
+print(a+b)
+print(a*b)
+print(type(a/b))
+print(type(a//b))
+print(type(a==b))
+print(type(a!=b))
+a = str("4")
+s = 'se'
+t = 'sa'
+print(s+t)
+print(t*5)
+print(s+t*4)
+print(s.capitalize)
